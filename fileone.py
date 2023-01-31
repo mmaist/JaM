ced54c84ca6d9e9dd4adbfe7e2ff1789874dac0c
mmaist <maxmaistros@gmail.com>
2023-01-30 20:46:38 -0800

    Added Dockerfile

--- fileone.py
+++ fileone.py
@@ -4,9 +4,28 @@ import json
 from google.cloud import storage
 from google.cloud import firestore
 import os
-os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "/Users/jackmaistros/peppy-sensor-375905-02f812598a1b.json"
+import functions_framework
 
-def getOdds(SPORT, API_KEY,REGIONS,MARKETS,ODDS_FORMAT,DATE_FORMAT,EVENT_ID):
+
+
+
+#app.run(port=int(os.environ.get("PORT", 8080)),host='0.0.0.0',debug=True)
+#os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "/Users/jackmaistros/peppy-sensor-375905-02f812598a1b.json"
+
+@functions_framework.http
+def listen(request):
+    nba_scores()
+
+def nba_scores():
+    SPORT = 'basketball_nba'
+    API_KEY = 'a55ffdec48c698cebf91b35126aa4269'
+    REGIONS = 'us'
+    MARKETS = 'h2h,spreads,totals'
+    ODDS_FORMAT = 'american'
+    DATE_FORMAT = 'iso'
+    DAYS_FROM = "2"
+    EVENT_ID = ''
+
     odds_response = requests.get(f'https://api.the-odds-api.com/v4/sports/{SPORT}/odds', params={
         'api_key': API_KEY,
         'regions': REGIONS,
@@ -17,42 +36,10 @@ def getOdds(SPORT, API_KEY,REGIONS,MARKETS,ODDS_FORMAT,DATE_FORMAT,EVENT_ID):
     })
 
     if odds_response.status_code != 200:
-        print(f'Failed to get odds: status_code {odds_response.status_code}, response body {odds_response.text}')
+        return f'Failed to get odds: status_code {odds_response.status_code}, response body {odds_response.text}'
     else:
         odds_json = odds_response.json()
-        print('Number of events:', len(odds_json))
-        printJsonListToFile(odds_json)
-
-    # Check the usage quota
-    #print('Remaining requests', odds_response.headers['x-requests-remaining'])
-    #print('Used requests', odds_response.headers['x-requests-used'])
-
-
-def printJsonListToFile(jsonlist):
-    file = open('test.txt','w')
-    db = firestore.Client(project='peppy-sensor-375905')
-    for jsonitem in jsonlist:
-        db.collection(u'NBA').document(str(jsonitem["id"])).set(jsonitem)
-    file.close()
-
-
-def main():
-    
-    API_KEY = 'a55ffdec48c698cebf91b35126aa4269'
-
-    SPORT = 'basketball_nba' 
-
-    REGIONS = 'us' 
-
-    MARKETS = 'h2h,spreads,totals' 
-
-    ODDS_FORMAT = 'american'
-
-    DATE_FORMAT = 'iso'
-
-    DAYS_FROM = "2"
-
-    EVENT_ID = '' 
-    getOdds(SPORT, API_KEY,REGIONS,MARKETS,ODDS_FORMAT,DATE_FORMAT,EVENT_ID)
-
-main()
+        db = firestore.Client(project='peppy-sensor-375905')
+        for jsonitem in odds_json:
+            db.collection(u'NBA').document(str(jsonitem["id"])).set(jsonitem)
+        return 'Data saved successfully to Firestore.'
