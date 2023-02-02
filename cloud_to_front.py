8e67c19e9cbcbf210707f6be10eeb249f00ffa27
Jack Maistros <jackmaistros@gmail.com>
2023-02-01 18:10:03 -0800

    Created a new file that allows to be called and then returns json with all upcoming NBA games

---
+++ cloud_to_front.py
@@ -0,0 +1,30 @@
+import requests
+import argparse
+import json
+from google.cloud import firestore
+import os
+import functions_framework
+from flask import Flask
+import datetime
+app = Flask(__name__)
+
+# Use a service account.
+@app.route('/getData')
+def getData():
+    
+    father_dict = {}
+    gamecounter = 0
+    db = firestore.Client(project='peppy-sensor-375905')
+
+    query = db.collection(u'NBA').order_by(u'commence_time', direction=firestore.Query.DESCENDING).limit(9).stream()
+
+    for doc in query:
+        gamecounter = gamecounter + 1
+        father_dict['game'+ (str(gamecounter))] = doc.to_dict()
+
+    json_return = json.dumps(father_dict, indent = 4)
+    return(json_return)
+    
+    #testing below
+    #with open("test.txt", "w") as outfile:
+    #   outfile.write(json_return)
