f9d31044cccc8ff5b34ca30a406646e6ef2abdb0
mmaist <maxmaistros@gmail.com>
2023-02-06 19:44:31 -0800

    added swr to grab data from localhost api

--- backend/cloud_to_front.py
+++ backend/cloud_to_front.py
@@ -9,6 +9,13 @@ import datetime
 app = Flask(__name__)
 
 # Use a service account.
+@app.after_request
+def after_request(response):
+  response.headers.add('Access-Control-Allow-Origin', '*')
+  response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+  response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+  return response
+
 @app.route('/getData')
 def getData():
     
