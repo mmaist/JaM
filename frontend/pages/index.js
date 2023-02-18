dc0a5e60fa67a0ccf2640f1eca0d28ce085f1145
Jack Maistros <jackmaistros@gmail.com>
2023-02-17 18:04:15 -0800

    First NEXTUI test

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -5,7 +5,31 @@ import styles from '@/styles/Home.module.css'
 import useSWR from 'swr'
 import moment from 'moment-timezone'
 import React from 'react';
+import { NextUIProvider } from '@nextui-org/react';
+import { Table } from "@nextui-org/react";
 
+// 1. Import `createTheme`
+import { createTheme, Text } from "@nextui-org/react"
+
+// 2. Call `createTheme` and pass your custom values
+const myDarkTheme = createTheme({
+  type: 'dark',
+  theme: {
+    colors: {
+      // brand colors
+      background: '#1d1d1d',
+      text: '#fff',
+      // you can also create your own color
+      myDarkColor: '#ff4ecd'
+      // ...  more colors
+    },
+    space: {},
+    fonts: {}
+  }
+})
+
+
+//Gathers data from our API and puts it into an array that then calls the display functions
 const fetcher = (...args) => fetch(...args).then((res) => res.json())
 
 function Games() {
@@ -23,6 +47,8 @@ function Games() {
 
  
 }
+
+///HTML verion of displaying the matchups
 function disGames(aGames){
 
   const gamesArray = Object.values(aGames)
@@ -49,7 +75,7 @@ function disGames(aGames){
     </div>
   );
 }
-
+//HTML Function to disiplay each games odds
 function disOdds(game) {
   const bookmakers = game.bookmakers;
   const columns = bookmakers.map((bookmaker) => {
@@ -80,6 +106,83 @@ function disOdds(game) {
   return <>{columns}</>;
 }
 
+//NEXTUI function to display the matchups
+function tabGames(aGames){
+const gamesArray=Object.values(aGames)
+return(
+    <Table
+      striped
+      sticked
+      aria-label="Example static striped collection table"
+      selectionMode="multiple"
+      css={{
+        height: "auto",
+        minWidth: "100%",
+      }}
+      >
+      <Table.Header>
+        <Table.Row>
+          <Table.Cell>Matchup</Table.Cell>
+          <Table.Cell>{moment.tz('America/Los_Angeles').format('MMM DD, YYYY h:mm A')}</Table.Cell>
+        </Table.Row>
+      </Table.Header>
+      <Table.Body>
+          {gamesArray.map((game) => (
+            <Table.Row key={game.id}>
+              <Table.Cell>{game.away_team} <br /> VS. {game.home_team}</Table.Cell>
+              <Table.Cell className={styles.myC}>{moment.utc(game.commence_time).tz('America/Los_Angeles').format('h:mm A')}</Table.Cell>
+              {tabOdds(game)}
+            </Table.Row>
+          ))}
+        </Table.Body>
+      </Table>
+
+
+      )
+}
+
+//Next UI function to display the odds
+function tabOdds(game) {
+  const bookmakers = game.bookmakers;
+  const columns = bookmakers.map((bookmaker) => {
+    return (
+      <Table.Cell key={bookmaker.key}>
+        <Table.Cell>{bookmaker.title}</Table.Cell>
+        <Table
+      striped
+      sticked
+      aria-label="Example static striped collection table"
+      selectionMode="multiple"
+      css={{
+        height: "auto",
+        minWidth: "100%",
+      }}
+      >
+          <Table.Body>
+            {bookmaker.markets.map((market) => (
+              <React.Fragment key={market.key}>
+                <Table.Row>
+                  <Table.Cell colSpan="2"><h3>{market.key}</h3></Table.Cell>
+                </Table.Row>
+                {market.outcomes.map((outcome) => (
+                  <Table.Row key={outcome.name}>
+                    <Table.Cell>{outcome.name}</Table.Cell>
+                    <Table.Cell>{outcome.price}</Table.Cell>
+                  </Table.Row>
+                ))}
+              </React.Fragment>
+            ))}
+          </Table.Body>
+        </Table>
+      </Table.Cell>
+    );
+  });
+
+  return <>{columns}</>;
+}
+
+
+///First thing that runs that sets up that page and then calls various functions
 export default function Home() {
   return (
     <>
@@ -89,10 +192,11 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div>
-        {Games()}
-
-      </div>
+      <NextUIProvider theme = {myDarkTheme}> {/* Wrap your top-level component with NextUIProvider */}
+        <div>
+          {Games()}
+        </div>
+      </NextUIProvider>
     </>
   )
 
