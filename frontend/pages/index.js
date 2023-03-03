221273ff752cbddb37630384183da27b7c25f6e4
mmaist <maxmaistros@gmail.com>
2023-03-03 15:26:09 -0800

    error catching

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -82,11 +82,39 @@ export default function Home() {
         <h4 style={{ position: 'absolute', top: 40, right: 10 }}>{timeDisplayer()}</h4>
       </Head>
       <div>
+      <ErrorBoundary>
         <NextUIProvider>{GamesFun(games, error, isLoading)}</NextUIProvider>
+      </ErrorBoundary>
       </div>
       
       
     </>
   )
 
+}
+
+class ErrorBoundary extends React.Component {
+constructor(props) {
+  super(props);
+  this.state = { hasError: false };
+}
+
+static getDerivedStateFromError(error) {
+  // Update state so the next render will show the fallback UI.
+  return { hasError: true };
+}
+
+componentDidCatch(error, errorInfo) {
+  // You can also log the error to an error reporting service
+  logErrorToMyService(error, errorInfo);
+}
+
+render() {
+  if (this.state.hasError) {
+    // You can render any custom fallback UI
+    return <h1>Something went wrong.</h1>;
+  }
+
+  return this.props.children; 
+}
 }
