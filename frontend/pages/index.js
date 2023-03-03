6ed499974004f7661492c65a8fdfd5e3bfbcc3d5
mmaist <maxmaistros@gmail.com>
2023-03-03 15:36:57 -0800

    more fixes

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -94,27 +94,35 @@ export default function Home() {
 }
 
 class ErrorBoundary extends React.Component {
-constructor(props) {
-  super(props);
-  this.state = { hasError: false };
-}
-
-static getDerivedStateFromError(error) {
-  // Update state so the next render will show the fallback UI.
-  return { hasError: true };
-}
-
-componentDidCatch(error, errorInfo) {
-  // You can also log the error to an error reporting service
-  console.log(error, errorInfo);
-}
-
-render() {
-  if (this.state.hasError) {
-    // You can render any custom fallback UI
-    return <h1>Something went wrong.</h1>;
+  constructor(props) {
+    super(props);
+    this.state = { error: null, errorInfo: null };
   }
-
-  return this.props.children; 
-}
+  
+  componentDidCatch(error, errorInfo) {
+    // Catch errors in any components below and re-render with error message
+    this.setState({
+      error: error,
+      errorInfo: errorInfo
+    })
+    // You can also log error messages to an error reporting service here
+  }
+  
+  render() {
+    if (this.state.errorInfo) {
+      // Error path
+      return (
+        <div>
+          <h2>Something went wrong.</h2>
+          <details style={{ whiteSpace: 'pre-wrap' }}>
+            {this.state.error && this.state.error.toString()}
+            <br />
+            {this.state.errorInfo.componentStack}
+          </details>
+        </div>
+      );
+    }
+    // Normally, just render children
+    return this.props.children;
+  }  
 }
